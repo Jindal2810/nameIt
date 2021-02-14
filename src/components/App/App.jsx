d258dad89bd7ac2ae19a5b683292a797d1981397
Muskan Jindal <muskanjindal2810@gmail.com>
2021-02-14 14:12:00 +0530

    Add the nameIt project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ResultsContainer from '../ResultsContainer/ResultsContainer';
+import SearchBox from '../SearchBox/SearchBox';
+import Header from './../Header/Header';
+import './App.css';
+
+const name=require('@rstacruz/startup-name-generator');
+
+ class App extends React.Component{
+
+    state={
+        headerText:'Name It!',
+        headerExpanded:true,
+        suggestedNames:[],
+    };
+     render(){
+         return (
+             
+             <div>
+                 
+            <Header headerExpanded={this.state.headerExpanded}
+            headTitle={this.state.headerText} />
+            <SearchBox onInputChange={this.handleInputChange} />
+            <ResultsContainer suggestedNames={this.state.suggestedNames}/>
+
+            
+             </div>
+         );
+     }
+     handleInputChange=(inputText)=>{
+        
+         this.setState({
+             headerExpanded:!inputText,
+             suggestedNames: inputText ? name(inputText) : [],
+         });
+     }
+ }
+ export default App;
